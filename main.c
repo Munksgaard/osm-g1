86fc5858b7ac41b4dfaeacae93dd0b4ed286bfce
Philip Munksgaard <pmunksgaard@gmail.com>
2011-02-08 15:17:21 +0100

    bedre test af to_array

--- main.c
+++ main.c
@@ -31,6 +31,15 @@ int main()
     printf("Size: %d\n", s);
 
     printf("\nTester to_array\n");
+    insert(&tree, 10);
+    insert(&tree, 7);
+    insert(&tree, 80);
+    insert(&tree, 1);
+    insert(&tree, 4);
+    insert(&tree, 3);
+    s = size(tree);
+    printf("Size: %d\n", s);
+    print_inorder(tree);
     int *arr = to_array(tree);
     for (int i = 0; i < s; ++i) {
         printf("arr[%d] = %d\n", i, arr[i]);
